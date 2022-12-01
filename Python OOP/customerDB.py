4372872b95a3119d42afc8c5577dde0550cdc9a2
UkemeSkywalker <skywalkershadow9@gmail.com>
2022-12-01 16:15:07 +0100

    User Interface functions

--- Python OOP/customerDB.py
+++ Python OOP/customerDB.py
@@ -47,40 +47,51 @@ class Customer :
 root = Tk()
 
 
-def Widgets(root):
+def main_widget(root):
     root.title("Create Customer")
 
-    title, name_label, name_e, membership_label, membership_e, submit_btn = create_widget()
+    title,name_label, name_e,membership_label, membership_e , submit_btn = create_widget()
+    
     Display(title, name_label, name_e, membership_label, membership_e, submit_btn)
+
+
+def create_widget( ):
+    title = Label(root, text="Customer Database", font=('Arial Bold', 30))
+    name_label = Label(root, text="Customer Name: ")
+    membership_label = Label(root, text="Membership Type: ")
+    name_e = Entry(root, width=50 )
+    membership_e = Entry(root, width=50 )
+    submit_btn = Button(root, text="Submit", width=20,  command=lambda: submit(name_e,membership_e ))   
+    print(membership_e.get(), "Line 64")
+
+    return title, name_label, name_e, membership_label, membership_e , submit_btn 
 
 
 def Display(title, name_label, name_e, membership_label, membership_e, submit_btn ):
     title.pack()
     name_label.pack()
     name_e.pack(pady=10)
+
     membership_label.pack()
     membership_e.pack(pady=10)
+
     submit_btn.pack(padx=5, pady=10)
 
-def create_widget( ):
-    title = Label(root, text="Customer Database", font=('Arial Bold', 30))
-    name_label = Label(root, text="Customer Name: ")
-    membership_label = Label(root, text="Membership Type: ")
-    submit_btn = Button(root, text="Submit", width=20,  command=submit)
-    name_e = Entry(root, width=50 )
-    membership_e = Entry(root, width=50 )
-    
-    return title, name_label, name_e, membership_label, membership_e, submit_btn
 
 def submit(name, membership):
     name_data = name.get()
     membership_data = membership.get()
-    return name_data, membership_data
+    Customer(name_data, membership_data)
+    print(name_data, membership_data)
+    success_message(name_data)
 
-Widgets(root)
 
-name_data, membership_data = submit()
-Customer(name_data, membership_data)
+def success_message(name):
+    
+    message = Label(root, text="New Customer "+name+" Added")
+    message.pack()
+
+main_widget(root)
 
 
 root.mainloop()
