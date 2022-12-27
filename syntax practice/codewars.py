df668cc3336875090c17a3be2460e9993619c8dd
UkemeSkywalker <skywalkershadow9@gmail.com>
2022-12-27 16:16:33 +0100

    updates

--- syntax practice/codewars.py
+++ syntax practice/codewars.py
@@ -26,3 +26,12 @@ def count_sheep2(sheeps):
     return sheeps.count(True)
 
 print(count_sheep2(sheeps))
+
+
+# Make a function that will return a greeting statement that uses an input; 
+# your program should return, "Hello, <name> how are you doing today?".
+
+# [Make sure you type the exact thing I wrote or the program may not execute properly]
+
+def greet(name):
+    return ("Hello, "+name+" how are you doing today?")
