66e95088bb3fd46b015f41cb0638d79a059cdbf7
UkemeSkywalker <skywalkershadow9@gmail.com>
2022-12-29 22:33:46 +0100

    more syntax practice

--- syntax practice/list.py
+++ syntax practice/list.py
@@ -1,24 +1,26 @@
 
-jobs = ["SRE", "Cloud Eng", "DevOps", 1, 2, 3, (4, 5), (6, 7), False]
+# jobs = ["SRE", "Cloud Eng", "DevOps", 1, 2, 3, (4, 5), (6, 7), False]
 
 
-### List methods
-#### add data to list - append(), extend() insert()
+# ### List methods
+# #### add data to list - append(), extend() insert()
 
-jobs.append(("Developer", 0.2, 16))
-jobs.extend((0.18, "Diamond cuts"))
-jobs.insert(0, "0.9")
+# jobs.append(("Developer", 0.2, 16))
+# jobs.extend((0.18, "Diamond cuts"))
+# jobs.insert(0, "0.9")
 
 
-### Remove data from list
-#### del, pop, remove
+# ### Remove data from list
+# #### del, pop, remove
 
-del jobs[0]
-print(jobs)
-x = jobs.pop()
-y = jobs.remove((4, 5))
-print(x)
-print(y)
-print(jobs)
-del jobs[6]
-print(jobs)
+# del jobs[0]
+# print(jobs)
+# x = jobs.pop()
+# y = jobs.remove((4, 5))
+# print(x)
+# print(y)
+# print(jobs)
+# del jobs[6]
+# print(jobs)
+
+
