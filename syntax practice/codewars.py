4d42d9979b713c8a044e4b52109701715c6c77dd
UkemeSkywalker <skywalkershadow9@gmail.com>
2022-12-31 21:19:13 +0100

    highest and lowest numbers

--- syntax practice/codewars.py
+++ syntax practice/codewars.py
@@ -104,3 +104,19 @@ arr = [4,6,2,1,9,63,-134,566]
 
 minimum(arr)
 maximum(arr)
+
+
+# In this little assignment you are given a string of space separated numbers, and have to return the 
+# highest and lowest number.
+
+def high_and_low(numbers):
+    num = list(map(int, numbers.split))
+    high = max(num)
+    low = min(num)
+
+    return f"{high} {low}"
+
+
+high_and_low("1 2 3 4 5")  # return "5 1"
+high_and_low("1 2 -3 4 5") # return "5 -3"
+high_and_low("1 9 3 4 -5") # return "9 -5"
