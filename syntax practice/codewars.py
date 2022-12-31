3c659aeb92b80d5442b3c745b60d9a2e3857b69c
UkemeSkywalker <skywalkershadow9@gmail.com>
2022-12-31 18:38:29 +0100

    make negative

--- syntax practice/codewars.py
+++ syntax practice/codewars.py
@@ -66,3 +66,19 @@ def find_needle(haystack): return 'found the needle at position %d' % haystack.i
 
 findNeedle(junk)
 
+
+
+# In this simple assignment you are given a number and have to make it negative. 
+# But maybe the number is already negative?
+
+def make_negative( number ):
+    if number == 0 :
+        return number
+    elif number > 0 :
+        return number - (number*2)
+    elif number < 0:
+        return number
+
+make_negative(1);  # return -1
+make_negative(-5); # return -5
+make_negative(0);  # return 0
